883478a25cba449a2f343f49306bc35e8ec2d137
kobaltdev <star.geec@gmail.com>
2024-06-26 06:14:28 +0200

    commit from pycharm

---
+++ perso/nbMagiqueAuto/main.py
@@ -0,0 +1,39 @@
+# imports
+import time
+
+
+def intro():
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+    print("Pensez à un nombre entier positif que je vais devoir trouver!")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *")
+    for i in range(3, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("C'est parti !")
+    print()
+
+
+def bornage():
+    borne_max = 10
+    print("Ok, je dois d'abord déterminer l'ordre de grandeur de votre nombre")
+    print("-----------")
+    while True:
+        reponse = input(f"Votre nombre est-il inférieur à {borne_max} ? (o / n) : ")
+        match reponse:
+            case "o":
+                break
+            case "n":
+                borne_max = borne_max * 10
+            case _:
+                print("Merci de répondre 'o' ou 'n'")
+    print()
+    print(f"Ok, votre nombre est inférieur à {borne_max} !")
+    return borne_max
+
+
+def trouver_nombre(borne_max):
+    borne_min = 0
+
+
+# intro()
+bmax = bornage()
